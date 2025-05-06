c89e08db8721a40d72c603cde380e45f11bcebdd
xieyuhan <xieyuhan@xieyuhandeMacBook-Pro.local>
2025-05-06 15:02:24 +0200

    add nav link

--- app/(box)/components/Nav.tsx
+++ app/(box)/components/Nav.tsx
@@ -1,7 +1,12 @@
+import Link from 'next/link';
+
 export const Nav = () => {
   return (
     <div className="header">
-      <h2 className="text-7xl h-full flex flex-col justify-center font-semibold">All in One</h2>
+      <Link href="/"><h2 className="text-7xl h-full flex flex-col justify-center font-semibold 
+        transition-all transform hover:scale-105 cursor-pointer 
+        bg-green-500 text-white px-6 py-4 rounded-lg 
+        border-2 border-transparent hover:border-green-700 hover:bg-green-600">All in One</h2></Link>
     </div>
   );
 };
