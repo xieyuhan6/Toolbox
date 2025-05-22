1efda74571d19c5c1a94bc09ab003b8192250d39
xieyuhan <xieyuhan@xieyuhandeMacBook-Pro.local>
2025-05-22 19:13:00 +0200

    update

--- app/backend/db.py
+++ app/backend/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL="postgresql://postgres:123456@db.bjpyqxacvzcmvhqpcnec.supabase.co:5432/postgres"
+DATABASE_URL="postgresql://postgres.bjpyqxacvzcmvhqpcnec:123456@aws-0-eu-north-1.pooler.supabase.com:6543/postgres"
 engine = create_engine(DATABASE_URL)
 Base = declarative_base()
 SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
